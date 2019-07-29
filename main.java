7037fbd9bffd2011ab6483d530d1f39fc36ec2c9
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-29 19:35:35 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,16 @@
+package Lincked_List;
+
+public class main {
+    public static void main(String []args){
+
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.InsertNode(3);
+        linkedList.InsertNode(7);
+        linkedList.InsertNode(9);
+
+        linkedList.Show();
+
+    }
+}
+
